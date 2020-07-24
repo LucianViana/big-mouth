44bb14eebbd36787a6530079cafbae0a10c1b1a2
Luciano <luciano.souzaviana@gmail.com>
2020-07-24 01:02:12 -0300

    atualização de mensagens para metricas do cloudwatch

--- functions/retry-notify-restaurant.js
+++ functions/retry-notify-restaurant.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const co     = require('co');
-const notify = require('../lib/notify');
-const log    = require('../lib/log');
+const co         = require('co');
+const notify     = require('../lib/notify');
+const log        = require('../lib/log');
+const cloudwatch = require('../lib/cloudwatch');
 
 const middy         = require('middy');
 const sampleLogging = require('../middleware/sample-logging');
+const flushMetrics  = require('../middleware/flush-metrics');
 
 const handler = co.wrap(function* (event, context, cb) {
   let order = JSON.parse(event.Records[0].Sns.Message);
@@ -29,4 +31,5 @@ const handler = co.wrap(function* (event, context, cb) {
 });
 
 module.exports.handler = middy(handler)
-  .use(sampleLogging({ sampleRate: 0.01 }));
+  .use(sampleLogging({ sampleRate: 0.01 }))
+  .use(flushMetrics);
